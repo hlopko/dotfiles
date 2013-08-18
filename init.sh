54f04b19f8767d30a6e1cc052a74fd1668771768
Marcel Hlopko <marcel.hlopko@gmail.com>
2013-08-18 19:34:33 +0200

    use xstow

--- init.sh
+++ init.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-hash stow 2>/dev/null || { echo >&2 "I require stow but it's not installed.  Aborting."; exit 1; }
+set -e
+
+hash xstow 2>/dev/null || { echo >&2 "I require stow but it's not installed.  Aborting."; exit 1; }
 
 packages=( ack bash bin gdb git hg ruby screen tmux vim zsh )
 
 if [[ $1 = "clean" ]]; then
 	ARGS="-D"
+else
+	ARGS="-f"
 fi
 
 
 for package in "${packages[@]}"
 do
-	stow $ARGS $package
+	xstow $ARGS $package
 done
 
-vim +BundleInstall +qall
-cd ~/.vim/bundle/command-t/ruby/command-t && ruby extconf.rb && make
+if [[ ! $1 = "clean" ]]; then
+	if [[ ! $1 = "fast" ]]; then
+		vim +BundleInstall +qall
+		cd ~/.vim/bundle/command-t/ruby/command-t && ruby extconf.rb && make
+	fi
+fi
 
 
