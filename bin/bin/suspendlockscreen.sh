35c12b3bd2b2f0285943bee8f2a12439748705ad
Marcel Hlopko <marcel.hlopko@gmail.com>
2014-10-23 23:23:19 +0200

    Update suspendlockscreen for new ubuntu

--- bin/bin/suspendlockscreen.sh
+++ bin/bin/suspendlockscreen.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-gnome-screensaver-command --lock
-dbus-send --system --print-reply --dest=org.freedesktop.UPower /org/freedesktop/UPower org.freedesktop.UPower.Suspend
+dbus-send --system --print-reply --dest="org.freedesktop.login1" /org/freedesktop/login1 org.freedesktop.login1.Manager.Suspend boolean:true
