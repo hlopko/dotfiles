73b5345b29b94f4e01a2e4399880b4455902cb95
Marcel Hlopko <marcel.hlopko@gmail.com>
2014-10-23 23:19:46 +0200

    Fix ebee projects script

--- bin/bin/ebee_projects_add_record.rb
+++ bin/bin/ebee_projects_add_record.rb
@@ -1,54 +1,66 @@
 #!/usr/bin/env ruby
 
-#ugly code warning
+# Ugly code warning
 
-require 'rubygems'
 require 'faraday'
 require 'faraday-cookie_jar'
 require 'optparse'
 require 'pry'
 
+def require_or_abort(field)
+  unless instance_variable_get(field)
+    puts "#{field[1..-1]} missing"
+    abort
+  end
+end
+
 OptionParser.new do |opts|
   opts.banner = "Usage: <script> [options]"
 
-  opts.on("-h hours",
-          "--hours=hours",
-          Integer,
-          "number of worked hours") do |arg|
-            @hours = arg
+  opts.on("-h hours", "--hours=hours", String, "number of worked hours") do |arg|
+    @hours = arg
   end
 
-  opts.on("-d date",
-          "--date=date",
-          String,
-          "date") do |arg|
-            @date = arg
+  opts.on("-d date", "--date=date", String, "date") do |arg|
+    @date = arg
   end
 
-  opts.on("-D desc",
-          "--Desc=desc",
-          String,
-          "description") do |arg|
-            @description = arg
+  opts.on("-p project", "--project=project", String, "project") do |arg|
+    @project = arg
+  end
+
+  opts.on("-i iteration", "--iteration=iteration", String, "iteration") do |arg|
+    @iteration = arg
+  end
+
+  opts.on("-w work_type", "--work-type=work_type", String, "work type") do |arg|
+    @work_type = arg
+  end
+
+  opts.on("-D desc", "--Desc=desc", String, "description") do |arg|
+    @description = arg
   end
 end.parse!
 
-
+[:@hours, :@date, :@project, :@iteration, :@work_type, :@description].each do |field|
+  require_or_abort field
+end
 
 HOST = "http://projects.ebee.cz"
 
 @faraday = Faraday.new(url: HOST) do |faraday|
-  faraday.request :url_encoded
-  faraday.adapter Faraday.default_adapter
   faraday.use :cookie_jar
+  faraday.request :url_encoded
+  #faraday.response :logger
+  faraday.adapter Faraday.default_adapter
 end
 
-get_response = @faraday.get("/process-login")
-get2_response = @faraday.get(get_response.headers["location"])
-
+make_me_session = @faraday.get("/login")
 login_response = @faraday.post("/process-login",
-           j_username: "hlopko@ebee.cz",
-           j_password: "WathfoshforHujShuneurgIk")
+                               _spring_security_remember_me: "on",
+                               j_username: "hlopko@ebee.cz",
+                               j_password: "WathfoshforHujShuneurgIk")
+redirected_homepage = @faraday.get("/work-records")
 
 work_record_response = @faraday.post("/work-records/new",
            date:  @date,
@@ -58,5 +70,4 @@ work_record_response = @faraday.post("/work-records/new",
            workDescription:  @description,
            workedHours:  @hours,
            hoursToBePaid:  @hours)
-
 
